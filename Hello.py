cc201a213a1c480109955dd86b29b468e8175514
vprameshdevops-source <vpramesh.devops@gmail.com>
2025-08-31 16:16:56 +0530

    commiting my code

--- Hello.py
+++ Hello.py
@@ -6,3 +6,18 @@ def find_sum(a):
     return sum
 
 print(find_sum(10))
+
+# Can you give me a pythron script to generate fibanacci series?
+
+def fibonacci(n):   
+    a, b = 0, 1
+    series = []
+    while len(series) < n:
+        series.append(a)
+        a, b = b, a + b
+    return series
+print(fibonacci(10))
+
+
+
+
